00448093360af3d51a7d20172ba27b2011534932
ktpunnisa <ktpunnisa@gmail.com>
2020-03-08 16:03:27 +0700

    add prettier

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import "kt-component";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import 'kt-component';
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule],
