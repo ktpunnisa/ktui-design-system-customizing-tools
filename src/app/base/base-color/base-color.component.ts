5a43b622298e241e91a64ff8807e88e99c671626
ktpunnisa <ktpunnisa@gmail.com>
2020-03-16 23:28:54 +0700

    add border for white palette

--- src/app/base/base-color/base-color.component.ts
+++ src/app/base/base-color/base-color.component.ts
@@ -13,6 +13,7 @@ export class BaseColorComponent implements OnInit {
   @Input() size = 's';
   palette: Palette;
   opacity: number;
+  colorGenerated: string;
 
   constructor(private DscColor: DscColorService) {}
 
@@ -21,15 +22,18 @@ export class BaseColorComponent implements OnInit {
   getColorStyle() {
     this.palette = this.DscColor.size[this.size];
     this.opacity = this.DscColor.dscShades[this.shade] * 0.01;
+    this.colorGenerated = this.DscColor.mixColorShade(
+      this.DscColor.dscColors[this.color],
+      this.shade,
+      this.opacity
+    );
     return {
-      'background-color': this.DscColor.mixColorShade(
-        this.DscColor.dscColors[this.color],
-        this.shade,
-        this.opacity
-      ),
+      'box-sizing': 'border-box',
+      'background-color': this.colorGenerated,
       width: `${this.palette.width}px`,
       height: `${this.palette.height}px`,
-      'border-radius': `${this.palette.borderRadius}px`
+      'border-radius': `${this.palette.borderRadius}px`,
+      border: this.colorGenerated === '#ffffff' || this.opacity === 1 ? '1px solid #E9E9E9' : 'none'
     };
   }
 }
