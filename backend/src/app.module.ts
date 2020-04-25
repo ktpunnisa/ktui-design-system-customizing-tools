9771cca822fa625b2287d1f82892ba3b0d89061e
ktpunnisa <ktpunnisa@gmail.com>
2020-04-25 23:06:19 +0700

    backend serve static file

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -19,6 +21,9 @@ import configuration from './configuration';
       load: [configuration],
       envFilePath: '.env',
       isGlobal: true,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../../../', 'db'),
     }),
     ProjectModule,
     ColorModule,
