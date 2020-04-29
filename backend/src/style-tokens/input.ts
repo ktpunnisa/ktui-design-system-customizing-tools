18abba4170edf774248b8683cf438281607b868d
ktpunnisa <ktpunnisa@gmail.com>
2020-04-29 22:47:45 +0700

    edit input token

--- backend/src/style-tokens/input.ts
+++ backend/src/style-tokens/input.ts
@@ -1,43 +1,28 @@
 export default {
   shape: {
-    label: {
-      margin: { bottom: 4 },
-    },
-    input: {
-      borderRadius: 4,
-      borderStyle: 'solid',
-      borderWidth: 1,
-    },
+    label: { margin: { bottom: 4 } },
+    input: { borderRadius: 4, borderStyle: 'solid', borderWidth: 2 },
   },
   sizes: {
     small: {
-      label: {
-        fontSize: 12,
-        fontWeight: 500,
-      },
+      label: { fontSize: 14, fontWeight: 400 },
       input: {
-        fontSize: 14,
-        padding: { top: 8, right: 12, bottom: 8, left: 12 },
+        fontSize: 16,
+        padding: { top: 6, right: 8, bottom: 6, left: 8 },
       },
     },
     medium: {
-      label: {
-        fontSize: 14,
-        fontWeight: 600,
-      },
+      label: { fontSize: 14, fontWeight: 500 },
       input: {
         fontSize: 16,
-        padding: { top: 8, right: 12, bottom: 8, left: 12 },
+        padding: { top: 8, right: 8, bottom: 8, left: 8 },
       },
     },
     large: {
-      label: {
-        fontSize: 14,
-        fontWeight: 700,
-      },
+      label: { fontSize: 16, fontWeight: 500 },
       input: {
         fontSize: 18,
-        padding: { top: 8, right: 12, bottom: 8, left: 12 },
+        padding: { top: 12, right: 10, bottom: 12, left: 10 },
       },
     },
   },
@@ -60,9 +45,9 @@ export default {
         },
       },
       disabled: {
-        label: { font: { type: 'fixed', color: 'gray', shade: 'light' } },
+        label: { font: { type: 'fixed', color: 'gray', shade: 'base' } },
         input: {
-          font: { type: 'fixed', color: 'gray', shade: 'lightest' },
+          font: { type: 'fixed', color: 'gray', shade: 'light' },
           background: { type: 'fixed', color: 'gray', shade: 'lightest' },
           border: { type: 'fixed', color: 'gray', shade: 'light' },
         },
